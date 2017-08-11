8c587ba37d110ce191f2e8ff029f425358d0d81d
Carter Shanklin <carter@HW11972.local>
2017-08-11 12:24:08 -0700

    Need a way to probe if the data is loaded in Druid or not.

--- 00load.sh
+++ 00load.sh
@@ -8,12 +8,4 @@ DRUID_PASSWORD=${5:-password}
 BEELINE_RAW="beeline -u jdbc:hive2://$HS2/ssb_${SCALE}_raw"
 BEELINE_ORC="beeline -u jdbc:hive2://$HS2/ssb_${SCALE}_flat_orc"
 
-# Don't do anything if the data is already loaded.
-hdfs dfs -ls /apps/hive/warehouse/ssb_${SCALE}_flat_orc.db >/dev/null
-
-if [ $? -ne 0 ];  then
-	# Load the data into Druid.
-	$BEELINE_ORC -f queries.druid/index_ssb.sql --hivevar DRUID_HOST=${DRUID_HOST} --hivevar DRUID_USERNAME=${DRUID_USERNAME} --hivevar DRUID_PASSWORD=${DRUID_PASSWORD} --hivevar SCALE=${SCALE} -n ${USER}
-else
-	echo "SSB Data already loaded."
-fi
+$BEELINE_ORC -f queries.druid/index_ssb.sql --hivevar DRUID_HOST=${DRUID_HOST} --hivevar DRUID_USERNAME=${DRUID_USERNAME} --hivevar DRUID_PASSWORD=${DRUID_PASSWORD} --hivevar SCALE=${SCALE} -n ${USER}
